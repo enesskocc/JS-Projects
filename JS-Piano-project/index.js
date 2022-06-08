d9fba9c02a9900201502e53378a15b2ae081164c
enes <enesskocc5@gmail.com>
2022-06-09 00:39:59 +0200

    Piano projesi eklendi.

---
+++ JS-Piano-project/index.js
@@ -0,0 +1,76 @@
+const ses = document.querySelector(".ses");
+const ses2 = document.querySelector(".ses2");
+const ses3 = document.querySelector(".ses3");
+const ses4 = document.querySelector(".ses4");
+const ses5 = document.querySelector(".ses5");
+const ses6 = document.querySelector(".ses6");
+const ses7 = document.querySelector(".ses7");
+const ses8 = document.querySelector(".ses8");
+const ses9 = document.querySelector(".ses9");
+const ses10 = document.querySelector(".ses10");
+const ses11 = document.querySelector(".ses11");
+const ses12 = document.querySelector(".ses12");
+const ses13 = document.querySelector(".ses13");
+
+
+
+document.querySelector("#ses1").onclick=()=>{
+    ses.play();
+    
+}
+
+document.querySelector("#ses2").onclick=()=>{
+    ses2.play();
+    
+}
+
+document.querySelector("#ses3").onclick=()=>{
+    ses3.play();
+    
+}
+document.querySelector("#ses3").onclick=()=>{
+    ses3.play();
+    
+}
+document.querySelector("#ses4").onclick=()=>{
+    ses4.play();
+    
+}
+document.querySelector("#ses5").onclick=()=>{
+    ses5.play();
+    
+}
+document.querySelector("#ses6").onclick=()=>{
+    ses6.play();
+    
+}
+document.querySelector("#ses7").onclick=()=>{
+    ses7.play();
+    
+}
+document.querySelector("#ses8").onclick=()=>{
+    ses8.play();
+    
+}
+document.querySelector("#ses9").onclick=()=>{
+    ses9.play();
+    
+}
+document.querySelector("#ses10").onclick=()=>{
+    ses10.play();
+    
+}
+document.querySelector("#ses11").onclick=()=>{
+    ses11.play();
+    
+}
+document.querySelector("#ses12").onclick=()=>{
+    ses12.play();
+    
+}
+document.querySelector("#ses13").onclick=()=>{
+    ses13.play();
+    
+}
+
+
